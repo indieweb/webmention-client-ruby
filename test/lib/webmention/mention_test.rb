bd7f2e82dad024bf88397b7027899a8ce4e045fd
Jason Garber <jason@sixtwothree.org>
2018-07-23 23:19:02 -0400

    Formatting updates to mention_test.rb

--- test/lib/webmention/mention_test.rb
+++ test/lib/webmention/mention_test.rb
@@ -2,37 +2,34 @@ require 'test_helper'
 
 describe Webmention::Client do
   before do
-    WebMock.stub_request(:any, 'http://source.example.com/post/100').to_return(
-      :status => 202,
-      :body => SampleData.sample_source_post_html,
-      :headers => {}
+    stub_request(:any, 'http://source.example.com/post/100').to_return(
+      status: 202,
+      body: SampleData.sample_source_post_html,
+      headers: {}
     )
 
-    WebMock.stub_request(:any, 'http://target.example.com/post/4').to_return(
-      :status => 202,
-      :body => SampleData.rel_webmention_href,
-      :headers => {}
+    stub_request(:any, 'http://target.example.com/post/4').to_return(
+      status: 202,
+      body: SampleData.rel_webmention_href,
+      headers: {}
     )
 
-    WebMock.stub_request(:any, 'http://target.example.com/post/5').to_return(
-      :status => 202,
-      :body => '',
-      :headers => {
-        'Link' => 'rel="webmention"; <http://webmention.io/example/webmention>'
+    stub_request(:any, 'http://target.example.com/post/5').to_return(
+      status: 202,
+      headers: {
+        'Link': 'rel="webmention"; <http://webmention.io/example/webmention>'
       }
     )
 
-    WebMock.stub_request(:any, 'http://webmention.io/example/webmention').to_return(
-      :status => 202
+    stub_request(:any, 'http://webmention.io/example/webmention').to_return(
+      status: 202
     )
-
-    @client = Webmention::Client.new "http://source.example.com/post/100"
   end
 
-  describe "#send_mentions" do
-    it "should return number of mentions sent" do
-      @client = Webmention::Client.new "http://source.example.com/post/100"
-      @client.send_mentions.must_equal 2
+  describe '#send_mentions' do
+    it 'should return number of mentions sent' do
+      client = Webmention::Client.new('http://source.example.com/post/100')
+      client.send_mentions.must_equal(2)
     end
   end
 end
