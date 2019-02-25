8da7405d33de82c6684d7d1aaafa6b7b2d8f9f54
Jason Garber <jason@sixtwothree.org>
2019-02-24 21:25:51 -0500

    Update test fixture markup

--- test/support/test_fixtures.rb
+++ test/support/test_fixtures.rb
@@ -4,10 +4,15 @@ module TestFixtures
       <head>
         <title>Sample Post</title>
       </head>
-      <body class="h-entry">
-        <p><a href="https://target.example.com/post/1">Link to Target 1</a></p>
-        <p><a href="https://target.example.com/post/2">Link to Target 2</a></p>
-        <p><img src="https://target.example.com/image.jpg" srcset="https://target.example.com/image-1x.jpg, https://target.example.com/image-2x.jpg 2x"></p>
+      <body>
+        <header>
+          <a href="/home" rel="home">Hello, world!</a>
+        </header>
+        <main class="h-entry">
+          <p><a href="https://target.example.com/post/1">Link to Target 1</a></p>
+          <p><a href="https://target.example.com/post/2">Link to Target 2</a></p>
+          <p><img src="https://target.example.com/image.jpg" srcset="https://target.example.com/image-1x.jpg, https://target.example.com/image-2x.jpg 2x"></p>
+        </main>
       </body>
     </html>'.freeze
 
@@ -16,9 +21,14 @@ module TestFixtures
       <head>
         <title>Sample Post</title>
       </head>
-      <body class="h-entry">
-        <p><a href="https://target.example.com/post/1">Link to Target 1</a></p>
-        <p><a href="https://target.example.com/post/2">Link to Target 2</a></p>
+      <body>
+        <header>
+          <a href="/home" rel="home">Hello, world!</a>
+        </header>
+        <main class="h-entry">
+          <p><a href="https://target.example.com/post/1">Link to Target 1</a></p>
+          <p><a href="https://target.example.com/post/2">Link to Target 2</a></p>
+        </main>
       </body>
     </html>'.freeze
 
@@ -27,8 +37,10 @@ module TestFixtures
       <head>
         <title>Sample Post</title>
       </head>
-      <body class="h-entry">
-        <p>Hello, world!</p>
+      <body>
+        <main class="h-entry">
+          <p>Hello, world!</p>
+        </main>
       </body>
     </html>'.freeze
 end
