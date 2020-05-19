2c587b0630a9f72df6c27659d0fa1554aa738003
Jason Garber <jason@sixtwothree.org>
2020-05-19 09:44:30 -0400

    Improve inline documentation

--- lib/webmention.rb
+++ lib/webmention.rb
@@ -17,13 +17,23 @@ require 'webmention/services/http_request_service'
 require 'webmention/services/node_parser_service'
 
 module Webmention
-  class << self
-    def client(source)
-      Client.new(source)
-    end
+  # Create a new Webmention::Client
+  # Convenience method for Webmention::Client.new
+  #
+  #   client = Webmention.client('https://source.example.com/post/100')
+  #
+  # @param source [String] An absolute URL representing the source document
+  # @return [Webmention::Client]
+  def self.client(source)
+    Client.new(source)
+  end
 
-    def send_mention(source, target)
-      client(source).send_mention(target)
-    end
+  # Send a webmention from the source URL to the target URL
+  #
+  # @param source [String] An absolute URL representing the source document
+  # @param target [String] An absolute URL representing the target document
+  # @return [HTTP::Response, nil]
+  def self.send_mention(source, target)
+    client(source).send_mention(target)
   end
 end
--- lib/webmention/client.rb
+++ lib/webmention/client.rb
@@ -1,5 +1,10 @@
 module Webmention
   class Client
+    # Create a new Webmention::Client
+    #
+    #   client = Webmention::Client.new('https://source.example.com/post/100')
+    #
+    # @param source [String] An absolute URL representing the source document
     def initialize(source)
       raise ArgumentError, "source must be a String (given #{source.class.name})" unless source.is_a?(String)
 
@@ -8,16 +13,28 @@ module Webmention
       raise ArgumentError, 'source must be an absolute URL (e.g. https://example.com)' unless source_uri.absolute?
     end
 
+    # Send webmentions to all mentioned URLs in this client's source document
+    #
+    # @return [Hash{String => HTTP::Response, nil}]
     def send_all_mentions
       mentioned_urls.each_with_object({}) { |url, hash| hash[url] = send_mention(url) }
     end
 
+    # Extract mentioned URLs from this client's source document
+    #
+    # @return [Array<String>]
+    # @raise [Webmention::UnsupportedMimeTypeError]
     def mentioned_urls
       raise UnsupportedMimeTypeError, "Unsupported MIME Type: #{source_response.mime_type}" unless parser_for_mime_type
 
       @mentioned_urls ||= parser_for_mime_type.new(source_response).results
     end
 
+    # Send a webmention from this client's source URL to the target URL
+    #
+    # @param target [String] An absolute URL representing the target document
+    # @return [HTTP::Response, nil]
+    # @raise [Webmention::ArgumentError, Webmention::ConnectionError, Webmention::InvalidURIError, Webmention::TimeoutError, Webmention::TooManyRedirectsError]
     def send_mention(target)
       endpoint = IndieWeb::Endpoints.get(target).webmention
 
