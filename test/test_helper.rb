4cdad6080a0478a65c710d938e78e040e6261b2c
Jason Garber <jason@sixtwothree.org>
2022-01-26 23:42:59 -0500

    Convert test support files to HTML

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,10 @@ require 'webmock/minitest'
 
 Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new
 
-require_relative 'support/test_fixtures'
+module TestFixtures
+  SAMPLE_POST_HTML = File.read(File.join(Dir.pwd, 'test', 'support', 'sample_post.html'))
+  SAMPLE_POST_HTML_ANCHORS_ONLY = File.read(File.join(Dir.pwd, 'test', 'support', 'sample_post_anchors_only.html'))
+  SAMPLE_POST_HTML_NO_LINKS = File.read(File.join(Dir.pwd, 'test', 'support', 'sample_post_no_links.html'))
+end
 
 require 'webmention'
