623e83b2678d9af3a40cbe39be3e7737f6602b83
Jason Garber <jason@sixtwothree.org>
2022-05-12 09:33:45 -0400

    Add module-level helper methods

--- lib/webmention.rb
+++ lib/webmention.rb
@@ -20,4 +20,53 @@ require_relative 'webmention/parsers/json_parser'
 require_relative 'webmention/parsers/plaintext_parser'
 
 module Webmention
+  # Retrieve unique URLs mentioned by the source URL.
+  #
+  # @example
+  #   Webmention.mentioned_urls('https://jgarber.example/posts/100')
+  #
+  # @param source [String, HTTP::URI, #to_s]
+  #   An absolute URL representing the source document.
+  #
+  # @raise [NoMethodError] Occurs when response is a Webmention::ErrorResponse.
+  # @raise [KeyError] Occurs when response if of an unsupported MIME type.
+  #
+  # @return [Array<String>]
+  def self.mentioned_urls(source)
+    Client.new(source).mentioned_urls
+  end
+
+  # Send a webmention from tthe source URL to the target URL.
+  #
+  # @example
+  #   source = 'https://jgarber.example/posts/100'
+  #   target = 'https://aaronpk.example/notes/1'
+  #   Webmention.send_webmention(source, target)
+  #
+  # @param source [String, HTTP::URI, #to_s]
+  #   An absolute URL representing the source document.
+  # @param target [String, HTTP::URI, #to_s]
+  #   An absolute URL representing the target document.
+  #
+  # @return [Webmention::Response, Webmention::ErrorResponse]
+  def self.send_webmention(source, target)
+    Client.new(source).send_webmention(target)
+  end
+
+  # Send webmentions from the source URL to the target URLs.
+  #
+  # @example
+  #   source = 'https://jgarber.example/posts/100'
+  #   targets = ['https://aaronpk.example/notes/1', 'https://adactio.example/notes/1']
+  #   Webmention.send_webmentions(source, targets)
+  #
+  # @param source [String, HTTP::URI, #to_s]
+  #   An absolute URL representing the source document.
+  # @param *targets [Array<String, HTTP::URI, #to_s>]
+  #   An array of absolute URLs representing the target documents.
+  #
+  # @return [Array<Webmention::Response, Webmention::ErrorResponse>]
+  def self.send_webmentions(source, *targets)
+    Client.new(source).send_webmentions(*targets)
+  end
 end
